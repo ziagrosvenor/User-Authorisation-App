cea64a92f1250256c0e3f0742919bbb5ca5e9ab1
ziagrosvenor <ziagrosvenor@live.co.uk>
2014-12-23 23:16:01 +0000

    Test routes, HTTP response headers.

--- components/coffee/test/Routes-Spec.coffee
+++ components/coffee/test/Routes-Spec.coffee
@@ -1,4 +1,5 @@
-app = require '../server'
+server = require '../server'
+app = require '../app'
 
 should = require 'should'
 
@@ -9,8 +10,24 @@ describe('authAppRoutes', () ->
 		supertest(app)
 		.get('/')
 		.expect(200)
-		.end (req, res) ->
-			res.status.should.equal(200)
-			.done()
+		.end (err, res) ->
+			res.status.should.equal(200);
+			done()
+	)
+	it('should issue a redirect message', (done) ->
+		supertest(app)
+		.post('/login')
+		.expect(302)
+		.end (err, res) ->
+			res.status.should.equal(302);
+			done()
+	)
+	it('should issue a page not found message', (done) ->
+		supertest(app)
+		.get('/kjdkdj')
+		.expect(404)
+		.end (err, res) ->
+			res.status.should.equal(404);
+			done()
 	)
 )
--- test/Routes-Spec.js
+++ test/Routes-Spec.js
@@ -1,15 +1,30 @@
-var app, should, supertest;
+var app, server, should, supertest;
 
-app = require('../server');
+server = require('../server');
+
+app = require('../app');
 
 should = require('should');
 
 supertest = require('supertest');
 
 describe('authAppRoutes', function() {
-  return it('should return 200 message for base URL', function(done) {
-    return supertest(app).get('/').expect(200).end(function(req, res) {
-      return res.status.should.equal(200).done();
+  it('should return 200 message for base URL', function(done) {
+    return supertest(app).get('/').expect(200).end(function(err, res) {
+      res.status.should.equal(200);
+      return done();
+    });
+  });
+  it('should issue a redirect message', function(done) {
+    return supertest(app).post('/login').expect(302).end(function(err, res) {
+      res.status.should.equal(302);
+      return done();
+    });
+  });
+  return it('should issue a page not found message', function(done) {
+    return supertest(app).get('/kjdkdj').expect(404).end(function(err, res) {
+      res.status.should.equal(404);
+      return done();
     });
   });
 });

2f09c484ddae8d1f7b07b32f93442bb7bef646bf
ziagrosvenor <ziagrosvenor@live.co.uk>
2014-12-26 16:48:08 +0000

    Re factor test server code. Pass database into app module as argument.

--- components/coffee/test/test-server.coffee
+++ components/coffee/test/test-server.coffee
@@ -1,4 +1,8 @@
+Db = require('../data/database.js')()
+
 app = require '../app'
+
+app = app(Db)
 
 server = app.listen 2999, () ->
 	console.log('Listening for tests')
--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,10 @@
-var app, server;
+var Db, app, server;
+
+Db = require('../data/database.js')();
 
 app = require('../app');
+
+app = app(Db);
 
 server = app.listen(2999, function() {
   return console.log('Listening for tests');

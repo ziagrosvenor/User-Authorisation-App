b3e778fe4e5bfede93a5247b4e41b65d69fe8169
ziagrosvenor <ziagrosvenor@live.co.uk>
2014-12-31 18:05:27 +0100

    Revert "Change passport user authentication. Use email instead of username. Use bcrypt to compare posted password against password stored in the database."
    This reverts commit 6ca2412a71b2ebf006ef5f0d29d9f2cd85ee1326.

--- auth.js
+++ auth.js
@@ -1,33 +1,31 @@
 module.exports = function(User) {
-  var LocalStrategy, bcrypt, passport;
+  var LocalStrategy, passport;
   passport = require('passport');
-  bcrypt = require('bcrypt');
   LocalStrategy = require('passport-local').Strategy;
   passport.use(new LocalStrategy(function(username, password, done) {
     return User.findOne({
-      email: username
+      username: username
     }, function(err, user) {
       if (err) {
         return done(err);
       }
       if (!user) {
-        console.log('no user');
         return done(null, false);
       }
-      if (bcrypt.compareSync(password, user.password) === false) {
+      if (user.password !== password) {
         return done(null, false, {
-          message: 'Unknown user #{user.firstname}'
+          message: 'Unknown user #{username}'
         });
       }
       return done(null, user);
     });
   }));
   passport.serializeUser(function(user, done) {
-    return done(null, user.email);
+    return done(null, user.username);
   });
-  passport.deserializeUser(function(email, done) {
+  passport.deserializeUser(function(username, done) {
     return done(null, {
-      username: email
+      username: username
     });
   });
   return passport;
